7b17fcdd2b98c9be302b990246c1dce58bf9d9f8
Ranveersingh66 <ranveersingh.pawar6627@gmail.com>
2026-08-04 23:52:56 +0530

    import export logic

---
+++ Export Import Logic/script.js
@@ -0,0 +1,8 @@
+import {addition as add} from './utility.js';
+
+const result = add(12, 23, 5);
+console.log(result); // logs 40
+
+import {multiply as multiply} from './utility.js';
+const result1 = multiply(23, 2);
+console.log(result1); // logs 46
---
+++ Export Import Logic/utility.js
@@ -0,0 +1,7 @@
+export function addition(a, b, c) {
+    return a + b + c;
+}
+
+export function multiply(q, w) {
+  return q * w;
+}
