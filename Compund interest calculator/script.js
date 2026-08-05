f296767b7009efcee89555fadf6b25bdd1823a6f
Ranveersingh66 <ranveersingh.pawar6627@gmail.com>
2026-08-05 17:55:36 +0530

    CIC

--- Compund interest calculator/script.js
+++ Compund interest calculator/script.js
@@ -1,9 +1,9 @@
-let P = 50000;
-const R = 0.12;
-const n = 1;
-let T = 5;
+let P = 50000; // Principal amount
+const R = 0.12; // Rate of Interest 
+const n = 1; // Number of times interest applied per time period (1 for annually)
+let T = 5; // Years
 
 let Amount = P * (1 + (R / n)) ** (n * T);
 
-console.log("the compound interest is after,", T, "years:", Amount);
- 
+console.log("the compound interest is after,", T, "years is:", Amount);
+  
