9f11e1eefba9bfb27dab54dc3fd94bc29bc8505d
Ranveersingh66 <ranveersingh.pawar6627@gmail.com>
2026-08-07 02:09:12 +0530

    Leap year calc

--- Conditional Statements (if else)/script.js
+++ Conditional Statements (if else)/script.js
@@ -35,7 +35,8 @@
 // else {
 //   console.log("You are not eligible to vote or drink alcohol!")
 // }
-
+ 
+// Grade evaluation using if else ladder
 const grade = 30;
 
 if (grade >= 90) {
@@ -51,3 +52,24 @@ else {
   console.log("Need to improve!")
 }
 
+// Traffic light using if else ladder
+
+const light = "green";
+if (light === "red") {
+  console.log("Stop!")
+}
+else if (light === "yellow") {
+  console.log("Get ready!")
+}
+else {
+  console.log("Go!")
+}
+
+// leap year using if else ladder
+const year = 2030;
+if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+  console.log("Leap year!")
+}
+else {
+  console.log("Not a leap year!")
+}
