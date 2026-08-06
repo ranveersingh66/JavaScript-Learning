a92129f7e1994d75973ad70f85027b2e833f0ba3
Ranveersingh66 <ranveersingh.pawar6627@gmail.com>
2026-08-07 01:37:10 +0530

    new

--- Conditional Statements (if else)/script.js
+++ Conditional Statements (if else)/script.js
@@ -1,12 +1,38 @@
-const candidateAge = 15;
-// If condition
-if (candidateAge >= 18) {
-  console.log("You are eligible to vote!")
-} 
+const candidateAge = 22;
+// // If condition
+// if (candidateAge >= 18) {
+//   console.log("You are eligible to vote!")
+// } 
 
-// If-Else condition
-if (candidateAge >= 18) {
-  console.log("You are eligible to vote!")
-} else {
-  console.log("You are not eligible to vote!")
+// // If-Else condition
+// if (candidateAge >= 18) {
+//   console.log("You are eligible to vote!")
+// } else {
+//   console.log("You are not eligible to vote!")
+// }
+
+// nested if condition
+// if (candidateAge >= 18) {
+//   console.log("You are eligible to vote!")
+
+//   if (candidateAge >= 21) {
+//     console.log("You are eligible to vote and drink alcohol!")
+//   }
+// }
+// else {
+
+//   console.log("You are not eligible to vote or drink alcohol!")
+// }
+
+// else if ladder condition
+
+if (candidateAge >= 21) {
+  console.log("You are eligible to vote and drink alcohol!")
 }
+else if (candidateAge >= 18) {
+  
+}
+else {
+  console.log("You are not eligible to vote or drink alcohol!")
+}
+
