39b68536a2f90ce6f1d64b385c236b6adef11287
Ranveersingh66 <ranveersingh.pawar6627@gmail.com>
2026-08-10 12:06:55 +0530

    switch

--- Switch JS/script.js
+++ Switch JS/script.js
@@ -1,7 +1,34 @@
-const name = "Switch JS";
-if (name === "Switch JS") {
-  console.log("The name is Switch JS");
-}
-else {
-  console.log("The name is not Switch JS");
+
+// // Grade evaluation using if else ladder
+// const grade = 30;
+
+// if (grade >= 90) {
+//   console.log("You got an A!")
+// }
+// else if (grade >= 75) {
+//   console.log ("You got a B!")
+// }
+// else if (grade >= 60) {
+//   console.log("You got a C!")
+// }
+// else {
+//   console.log("Need to improve!")
+// }
+
+// switch in JS
+ 
+const marks = 90;
+
+switch(marks) {
+  case 90:
+    console.log("You got an A!");
+    break;
+  case 75:
+    console.log("You got a B!");
+    break;
+  case 60:
+    console.log("You got a C!");
+    break;
+  default:
+    console.log("Need to improve!");
 }
