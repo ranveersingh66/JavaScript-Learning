e6b317f593552bcca3b717069f49e72e09d998df
Ranveersingh66 <ranveersingh.pawar6627@gmail.com>
2026-08-10 23:20:41 +0530

    loops1

---
+++ Loops in JS/script.js
@@ -0,0 +1,6 @@
+ // For loops in JavaScript
+ // A for loop is a control structure that allows you to repeat a block of code a certain number of times. It consists of three parts: 
+ // INITIALIZATION, CONDITION, and INCREMENT/DECREMENT. The syntax of a for loop is as follows:
+ // for (initialization; condition; increment/decrement) {
+ //     // code to be executed
+ // }
