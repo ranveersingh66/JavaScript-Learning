7bfc11b980111755baed84734fa82abb7d6eb345
Ranveersingh66 <ranveersingh.pawar6627@gmail.com>
2026-08-15 02:36:34 +0530

    loop practice

--- Loops in JS/script.js
+++ Loops in JS/script.js
@@ -15,6 +15,21 @@
 //     console.log(x); // Output: 1, 3, 5
 // }
 
-for(a=15; a>0; a--){
-    console.log(a); // Output: 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+// for(a=15; a>0; a--){
+//     console.log(a); // Output: 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+// } 
+
+let a = 0;
+for(a; a < 11; a++){
+    console.log(a); // Output: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+}
+
+let x = 0;
+for(x; x<20; x+=2){
+    console.log(x); // Output: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18
+}
+
+let z = 5;
+for(z; z<=50; z+=5){
+    console.log(z); // Output: 5, 10, 15, 20, 25, 30, 35, 40, 45, 50
 }
