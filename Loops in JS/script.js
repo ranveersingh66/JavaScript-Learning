2c01f1090601a9cc4ade18a566e8c02f97f5a863
Ranveersingh66 <ranveersingh.pawar6627@gmail.com>
2026-08-15 05:10:20 +0530

    new

--- Loops in JS/script.js
+++ Loops in JS/script.js
@@ -19,17 +19,24 @@
 //     console.log(a); // Output: 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
 // } 
 
-let a = 0;
-for(a; a < 11; a++){
-    console.log(a); // Output: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+// let a = 0;
+// for(a; a < 11; a++){
+//     console.log(a); // Output: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+// }
+
+// let x = 0;
+// for(x; x<20; x+=2){
+//     console.log(x); // Output: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18
+// }
+
+// let z = 5;
+// for(z; z<=50; z+=5){
+//     console.log(z); // Output: 5, 10, 15, 20, 25, 30, 35, 40, 45, 50
+// }
+
+let t = 0; 
+while(t<10){ // The while loop is a control structure that allows you to repeat a block of code as long as a specified condition is true. The syntax of a while loop is as follows:
+    console.log(t);
+    t++; // Output: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 }
 
-let x = 0;
-for(x; x<20; x+=2){
-    console.log(x); // Output: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18
-}
-
-let z = 5;
-for(z; z<=50; z+=5){
-    console.log(z); // Output: 5, 10, 15, 20, 25, 30, 35, 40, 45, 50
-}
