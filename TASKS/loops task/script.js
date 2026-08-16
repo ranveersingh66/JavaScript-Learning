ddd3475e744300ecc062d0886a241ec823613f4a
Ranveersingh66 <ranveersingh.pawar6627@gmail.com>
2026-08-16 23:17:05 +0530

    task

--- TASKS/loops task/script.js
+++ TASKS/loops task/script.js
@@ -1,1 +1,18 @@
+const n = 12;
+  console.log("Ex. Number:", n);
+// Sum of first n numbers
+let sum = (n*(n+1))/2
+  console.log("Sum of first n numbers:", sum)
+
+// table of n
+for(x=12; x<=120; x+=12){
+  console.log(x)
+}
+
+if(n == ){
+
+}
+
+
+
 
