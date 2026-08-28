8e7c47ac0f466edd309b3933c70c0369757f39d6
Ranveersingh66 <ranveersingh.pawar6627@gmail.com>
2026-08-28 20:25:46 +0530

    arrays

--- Arrays in js/script.js
+++ Arrays in js/script.js
@@ -1,1 +1,12 @@
 // arrays in js
+
+let arrayName = [123, 345, 567,] // can store multiple  values in a sungle array
+//index number  0,   1,   2, ...    (Index number starts always starts from 0)   
+
+console.log(arrayName); 
+// 0: 123
+// 1: 345
+// 2: 567
+
+console.log(arrayName[2]);
+// this prints only the 2nd value in the array
