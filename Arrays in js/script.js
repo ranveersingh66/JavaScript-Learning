7592a867933b888a033244497b3dead6648b58eb
Ranveersingh66 <ranveersingh.pawar6627@gmail.com>
2026-08-31 02:30:04 +0530

    arrays2

--- Arrays in js/script.js
+++ Arrays in js/script.js
@@ -1,4 +1,4 @@
-// arrays in js
+// An array is a data structure to store multiple items in a specific sequence
 
 let arrayName = [123, 345, 567,] // can store multiple  values in a sungle array
 //index number  0,   1,   2, ...    (Index number starts always starts from 0)   
@@ -10,3 +10,9 @@ console.log(arrayName);
 
 console.log(arrayName[2]);
 // this prints only the 2nd value in the array
+ 
+let arr = [123, "Hey", 345, 2390,] //an array can contain both string and integers 
+console.log(arr.length) // prints the length of the following array i.e. 4
+
+arr[2] = "my name is ranveer" // changes the value of index number 2 in the follwoing array.
+ 
