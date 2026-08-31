79d5e0aead7605d42c8efe24edfe12435e413687
Ranveersingh66 <ranveersingh.pawar6627@gmail.com>
2026-08-31 12:49:26 +0530

    arrays complete

--- Arrays in js/script.js
+++ Arrays in js/script.js
@@ -15,4 +15,24 @@ let arr = [123, "Hey", 345, 2390,] //an array can contain both string and intege
 console.log(arr.length) // prints the length of the following array i.e. 4
 
 arr[2] = "my name is ranveer" // changes the value of index number 2 in the follwoing array.
+
+console.log(arr.indexOf(123)); // tells the index number of the following value i.e. 0
+
+console.log(arr.includes("Hey")) // give a boolean value if that value exists in the array
+ arr.push("add this on the array!") // add the following value/string to the end of the array
+ arr.unshift("add this on the start") // adds the value to the start of the array
+ arr.pop() // deletes the last element of the following array
+ arr.shift() // deletes the first element of the following array
+
+let marks = [12, 23, 45, 98, 75, 1, 98, 11]
+// index no.    0,  1,  2,  3,  4,  5,  6,  7
  
+// marks.sort() // sorts the following array (Ascending order)
+
+let SubarrMarks = marks.slice(1,5) // prints the values between index number 2 and 6
+console.log(SubarrMarks);
+
+
+
+
+
