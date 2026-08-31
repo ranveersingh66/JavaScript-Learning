11298bccf654e2a285a84fc8b82e92767bd3fbc4
Ranveersingh66 <ranveersingh.pawar6627@gmail.com>
2026-08-31 13:15:06 +0530

    functions start

---
+++ Functions in Js/script.js
@@ -0,0 +1,11 @@
+// Some code snippet
+let a = 23;
+let b = 21;
+const aPlusb = a + b;
+console.log("Result:", aPlusb);
+
+// Some other code snippet
+let x = 10;
+let y = 12;
+const xPlusy = x + y;
+console.log("Result:", xPlusy);
