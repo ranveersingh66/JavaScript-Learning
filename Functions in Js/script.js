69c98a1be7441cb8649f5fb028cf4288ffa49222
Ranveersingh66 <ranveersingh.pawar6627@gmail.com>
2026-09-01 11:37:05 +0530

    functions2

--- Functions in Js/script.js
+++ Functions in Js/script.js
@@ -1,11 +1,30 @@
 // Some code snippet
-let a = 23;
-let b = 21;
+const a = 23;
+const b = 21;
 const aPlusb = a + b;
 console.log("Result:", aPlusb);
 
 // Some other code snippet
-let x = 10;
-let y = 12;
+const x = 10;
+const y = 12; 
 const xPlusy = x + y;
 console.log("Result:", xPlusy);
+
+//some other code snippet 
+const k = 5;
+const l = 12;
+const kPlusl = k + l;
+console.log("Result:", kPlusl);
+
+// Function Declaration
+function sumation(q, w){
+  const result = q + w;
+  console.log("Result:", result);
+}
+// Function call
+sumation(10, 43);
+
+function greet(){
+  console.log("Hello, welcome to the world of JavaScript!");
+}
+greet();
