a33e70018045b472a777a4ca78fc2584b2c7c4ed
Ranveersingh66 <ranveersingh.pawar6627@gmail.com>
2026-09-21 12:42:16 +0530

    array task

--- Conditional Statements (if else)/script.js
+++ Conditional Statements (if else)/script.js
@@ -1,40 +1,40 @@
-// const candidateAge = 22;
-// // If condition
-// if (candidateAge >= 18) {
-//   console.log("You are eligible to vote!")
-// } 
+const candidateAge = 22;
+// If condition
+if (candidateAge >= 18) {
+  console.log("You are eligible to vote!")
+} 
 
-// // If-Else condition
-// if (candidateAge >= 18) {
-//   console.log("You are eligible to vote!")
-// } else {
-//   console.log("You are not eligible to vote!")
-// }
+// If-Else condition
+if (candidateAge >= 18) {
+  console.log("You are eligible to vote!")
+} else {
+  console.log("You are not eligible to vote!")
+}
 
 // nested if condition
-// if (candidateAge >= 18) {
-//   console.log("You are eligible to vote!")
+if (candidateAge >= 18) {
+  console.log("You are eligible to vote!")
 
-//   if (candidateAge >= 21) {
-//     console.log("You are eligible to vote and drink alcohol!")
-//   }
-// }
-// else {
+  if (candidateAge >= 21) {
+    console.log("You are eligible to vote and drink alcohol!")
+  }
+}
+else {
 
-//   console.log("You are not eligible to vote or drink alcohol!")
-// }
+  console.log("You are not eligible to vote or drink alcohol!")
+}
 
 // else if ladder condition
 
-// if (candidateAge >= 21) {
-//   console.log("You are eligible to vote and drink alcohol!")
-// }
-// else if (candidateAge >= 18) {
-//   console.log("You are eligible to vote!")
-// }
-// else {
-//   console.log("You are not eligible to vote or drink alcohol!")
-// }
+if (candidateAge >= 21) {
+  console.log("You are eligible to vote and drink alcohol!")
+}
+else if (candidateAge >= 18) {
+  console.log("You are eligible to vote!")
+}
+else {
+  console.log("You are not eligible to vote or drink alcohol!")
+}
  
 // Grade evaluation using if else ladder
 const grade = 30;
---
+++ TASKS/arrays/script.js
@@ -0,0 +1,36 @@
+const arr = [22, 98, 56, 74, 88]
+
+// Maximum number
+let maxVal = arr.sort()
+console.log("Maximum value: ", maxVal[4]);
+
+// Sum of all numbers
+let a = arr[0];
+let b = arr[1];
+let c = arr[2];
+let d = arr[3];
+let e = arr[4];
+const sum = a + b + c + d + e
+console.log("Sum of all elements: ", sum);
+
+// Count of odd nunmbers
+// let q = arr[0];
+// let w = arr[1];
+// let r = arr[2];
+// let t = arr[3];
+// let y = arr[4];
+
+// if (q/2 === 0 ) {
+//   console.log(q, "Is not an odd number");
+// }
+// else{
+
+// }
+
+const oddnum = arr.filter()
+
+
+
+
+
+
