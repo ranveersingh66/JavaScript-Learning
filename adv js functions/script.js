b15fc9f6ffa09dabc3a65bfa50645f244abff352
Ranveersingh66 <ranveersingh.pawar6627@gmail.com>
2026-09-22 08:50:34 +0530

    Advance Functions part 1

---
+++ adv js functions/script.js
@@ -0,0 +1,12 @@
+// Named Functions
+function greet(name){
+  console.log("Hello", name);
+}
+greet("ranveer")
+
+// Anonymous Func
+let greeting = function(){
+  console.log("Hey Everyone!");
+}
+greeting()
+
